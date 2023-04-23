6e6671c2789c5709237dd62cdab3bc8ecf73b588
0x00A0 <ramon_g_wan@outlook.com>
2023-04-23 21:04:42 +0800

    Midterm Project

---
+++ js/iUp.js
@@ -0,0 +1,28 @@
+var iUp = (function () {
+	var time = 0,
+		duration = 150,
+		clean = function () {
+			time = 0;
+		},
+		up = function (element) {
+			setTimeout(function () {
+				element.classList.add("up");
+			}, time);
+			time += duration;
+		},
+		down = function (element) {
+			element.classList.remove("up");
+		},
+		toggle = function (element) {
+			setTimeout(function () {
+				element.classList.toggle("up");
+			}, time);
+			time += duration;
+		};
+	return {
+		clean: clean,
+		up: up,
+		down: down,
+		toggle: toggle
+	};
+})();
